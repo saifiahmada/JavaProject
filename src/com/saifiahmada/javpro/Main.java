7d6984499b282673ce7b0e748a6470857e186096
Saifi Ahmada <saifiahmada@gmail.com>
2013-04-15 14:13:17 +0800

    Commit Java Project

---
+++ src/com/saifiahmada/javpro/Main.java
@@ -0,0 +1,10 @@
+package com.saifiahmada.javpro;
+
+/** @author Saifi Ahmada Apr 15, 2013 2:10:26 PM  **/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Ini main method bro");
+	}
+}
+
